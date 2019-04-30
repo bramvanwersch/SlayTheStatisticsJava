7c69c5eb2b621c54ccdea84902060135f9539305
Bram <eckelradekip10@gmail.com>
2019-04-30 19:37:39 +0100

    refined the graph. Still lots of bugs but can produce a working version

--- SlayTheStatistics/src/gui/GraphBuilder.java
+++ SlayTheStatistics/src/gui/GraphBuilder.java
@@ -25,7 +25,7 @@ public class GraphBuilder{
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				new GraphBuilder(new int[] {1,2,30,44}, new int[] {1,24,3,4});
+				new GraphBuilder(new int[] {0,1,2,3,34}, new int[] {0,1,2,3,40});
 				}
 			});
 	}
@@ -38,9 +38,9 @@ public class GraphBuilder{
 
 	protected void createNewGui() {
 		SwingUtilities.isEventDispatchThread();
-        JFrame f = new JFrame("Swing Paint Demo");
+        JFrame f = new JFrame("Graph");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new MyPanel(xData,yData, 400, 300));
+        f.add(new MyPanel(xData,yData, 1000	, 1000));
         f.pack();
         f.setVisible(true);
     }
@@ -50,9 +50,11 @@ class MyPanel extends JPanel {
 	private int[] yData;
 	private int pHeigth;
 	private int pWidth;
-	private int DISTANCE_BORDER = 30;
-	private int NUMBER_DISTANCE = 15;
+	private int DISTANCE_BORDER = 50;
+	private int NUMBER_DISTANCE = 20;
 	private int NUMBER_OF_AXIS_POINTS = 10;
+	private int AXIS_STRIPE_LENGHT = 5;
+	private int SIZE_OF_POINTS = 7;
 
     public MyPanel(int[] xData, int[] yData,int width,int height) {
     	this.xData =xData;
@@ -69,9 +71,27 @@ class MyPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawGraphGrid(g);
+        drawGraphLine(g);
+    }
+    
+    public void drawGraphLine(Graphics g) {
+      	double xMax = max(xData);
+    	double yMax = closestUpperTenVal(max(yData));
+    	//get the amount of pixels per 1 number. 
+    	double noToPixelX = (pWidth - 2*DISTANCE_BORDER) / xMax;
+    	double noToPixelY = (pHeigth- 2*DISTANCE_BORDER) / yMax;
         for (int i = 0; i < xData.length-1; i++) {
-            g.drawLine(xData[i],yData[i],xData[i+1],yData[i+1]);
+        	g.setColor(Color.RED);
+        	int x1 = (int) (xData[i]*noToPixelX + DISTANCE_BORDER);
+        	int x2 = (int) (xData[i+1]*noToPixelX + DISTANCE_BORDER);
+        	int y1 = (int) ((pHeigth - yData[i]*noToPixelY) - DISTANCE_BORDER);
+        	int y2 = (int) ((pHeigth -yData[i+1]*noToPixelY) - DISTANCE_BORDER);
+            g.drawLine(x1, y1, x2, y2);
+            g.setColor(Color.BLACK);
+            g.fillOval(x1 - SIZE_OF_POINTS/2, y1 - SIZE_OF_POINTS/2,SIZE_OF_POINTS, SIZE_OF_POINTS);
+            g.fillOval(x2 - SIZE_OF_POINTS/2, y2 - SIZE_OF_POINTS/2,SIZE_OF_POINTS, SIZE_OF_POINTS);
         }
+    	
     }
     
     public void drawGraphGrid(Graphics g) {
@@ -81,7 +101,7 @@ class MyPanel extends JPanel {
     	//values on axis
     	int xLineLen = pWidth - 2* DISTANCE_BORDER;
     	int yLineLen = pHeigth - 2*DISTANCE_BORDER;
-    	int xMax = closestUpperTenVal(max(xData));
+    	int xMax = max(xData);
     	int yMax = closestUpperTenVal(max(yData));
     	//draw the 0
     	g.drawString("0", DISTANCE_BORDER, pHeigth - DISTANCE_BORDER + NUMBER_DISTANCE );
@@ -89,18 +109,28 @@ class MyPanel extends JPanel {
 
     	int count = 1;
     	//x axis numbers
-    	for (int i = xMax/NUMBER_OF_AXIS_POINTS; i <= xMax; i += xMax/10) {
-    		g.drawString(i+ "", DISTANCE_BORDER + (xLineLen/NUMBER_OF_AXIS_POINTS)*count 
-    				,pHeigth - DISTANCE_BORDER + NUMBER_DISTANCE);
+    	for (int i = xMax/NUMBER_OF_AXIS_POINTS; i < xMax; i += xMax/10) {
+    		int xCoord = DISTANCE_BORDER + (xLineLen/NUMBER_OF_AXIS_POINTS)*count;
+    		int yCoord = pHeigth - DISTANCE_BORDER;
+    		drawCenteredString(xCoord, yCoord + NUMBER_DISTANCE, i+"",g);
+    		g.drawLine(xCoord, yCoord + AXIS_STRIPE_LENGHT, xCoord, yCoord);
     		count ++;
     	}
     	//y axis numbers
     	count = 1;
     	for (int i = yMax/NUMBER_OF_AXIS_POINTS; i <= yMax; i += yMax/10) {
-    		g.drawString(i +"",DISTANCE_BORDER - NUMBER_DISTANCE,
-    				pHeigth - DISTANCE_BORDER - (yLineLen/NUMBER_OF_AXIS_POINTS)*count);
+    		int xCoord = DISTANCE_BORDER;
+    		int yCoord = pHeigth - DISTANCE_BORDER - (yLineLen/NUMBER_OF_AXIS_POINTS)*count;
+    		drawCenteredString(xCoord- NUMBER_DISTANCE, yCoord, i+"", g);
+    		g.drawLine(xCoord, yCoord, xCoord - AXIS_STRIPE_LENGHT, yCoord);
     		count++;
     	}
+    }
+    
+    private void drawCenteredString(int xCoord, int yCoord, String text, Graphics g) {
+    	int adjXCoord = xCoord - g.getFontMetrics().stringWidth(text)/ 2;
+    	int adjYCoord = yCoord  + g.getFontMetrics().getHeight()/3;
+    	g.drawString(text, adjXCoord, adjYCoord);
     }
     
     private int max(int[] data) {

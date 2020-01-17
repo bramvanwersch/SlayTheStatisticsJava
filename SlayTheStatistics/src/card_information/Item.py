d9b2c0a1024c0530d60f48b2c82d96daf9f663d7
Bram <eckelradekip10@gmail.com>
2020-01-17 17:26:39 +0100

    and other class

---
+++ SlayTheStatistics/src/card_information/Item.py
@@ -0,0 +1,11 @@
+
+
+class Item:
+
+    def __init__(self, info):
+        self.name,\
+        self.rarity,\
+        self.type,\
+        self.mana,\
+        self.description,\
+        self.character = info

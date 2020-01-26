95817628e7fb73063466acf9213d9970365e3932
Bram <eckelradekip10@gmail.com>
2020-01-26 20:45:49 +0100

    annotated matrix class

--- SlayTheStatistics/src/Utilities/Matrix.java
+++ SlayTheStatistics/src/Utilities/Matrix.java
@@ -1,6 +1,5 @@
 package Utilities;
 
-import java.awt.EventQueue;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +12,33 @@ public class Matrix {
 		this.size = new int[] {cols, rows};
 	}
 	
+	/**
+	 * Adds a column class to the matrix if the maximum amount of columns
+	 * was not reached yet.
+	 * @param data to add to the column
+	 */
 	public void addColumn(Object[] data) {
+		checkColumnSize();
 		addColumn(data, columns.size());
 	}
 	
+	/**
+	 * Adds a column class to the matrix at a given index if the maximum
+	 * amount of columns was not reached yet.
+	 * @param data to add to the column
+	 * @param index of the column to be added to.
+	 */
 	public void addColumn(Object[] data, int index) {
 		checkColumnSize();
 		columns.add(index, new Column(size[1]));
 		addColumnData(index, data);
 	}
 	
+	/**
+	 * adds an array of objects to a column.
+	 * @param index the index of the column
+	 * @param data object array containing any class to be added to the column.
+	 */
 	private void addColumnData(int index, Object[] data) {
 		checkRowSize(data.length);
 		columns.get(index).setData(data);
@@ -40,6 +56,11 @@ public class Matrix {
 		return (Any) columns.get(x).get(y);
 	}
 	
+	/**
+	 * Returns a column by index as an array of the given type.
+	 * @param x the column to be returned
+	 * @return a array of the requested type.
+	 */
 	@SuppressWarnings("unchecked")
 	public <Any> Any get(int x) {
 		return (Any) columns.get(x).get();
@@ -55,6 +76,10 @@ public class Matrix {
 		}
 	}
 	
+	/**
+	 * checks if the column that is being added is of the right size. All rows need to be the same size
+	 * @param l the lenght of the column being added.
+	 */
 	private void checkRowSize(int l) {
 		if (l != size[1]) {
 			throw new IndexOutOfBoundsException("Column data is to long or to short."
